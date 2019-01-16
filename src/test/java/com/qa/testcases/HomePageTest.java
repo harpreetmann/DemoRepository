cf1f0cfd86b7c0885d1e7e9f838c05688fcdf352
Your Name <you@example.com>
2019-01-16 11:40:14 -0500

    second commit

---
+++ src/test/java/com/qa/testcases/HomePageTest.java
@@ -0,0 +1,14 @@
+package com.qa.testcases;
+
+import org.testng.annotations.Test;
+
+import com.qa.base.TestBase;
+
+public class HomePageTest extends TestBase {
+	@Test
+	public void setup()
+	{
+	System.out.println("home page test");
+
+}
+}
